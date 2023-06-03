ca675327c72543da89735160f9f57b8cbc7237c4
khudson13 <65306661+khudson13@users.noreply.github.com>
2023-06-02 17:11:31 -0700

    hello world

---
+++ Ravening_Toad.py
@@ -0,0 +1,44 @@
+# Kendall Hudson
+# SDEV-435
+# THE RAVENING TOAD
+
+
+#!/usr/bin/env python3
+
+import tcod.console
+import tcod.context
+import tcod.event
+import tcod.tileset
+
+WIDTH, HEIGHT = 80, 60  # Console width and height in tiles.
+
+
+def main() -> None:
+    """Script entry point."""
+    # Load the font, a 32 by 8 tile font with libtcod's old character layout.
+    tileset = tcod.tileset.load_tilesheet(
+        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD,
+    )
+    # Create the main console.
+    console = tcod.console.Console(WIDTH, HEIGHT, order="F")
+    # Create a window based on this console and tileset.
+    with tcod.context.new(  # New window for a console of size columns×rows.
+        columns=console.width, rows=console.height, tileset=tileset,
+    ) as context:
+        while True:  # Main loop, runs until SystemExit is raised.
+            console.clear()
+            console.print(x=0, y=0, string="Hello World!")
+            context.present(console)  # Show the console.
+
+            # This event loop will wait until at least one event is processed before exiting.
+            # For a non-blocking event loop replace `tcod.event.wait` with `tcod.event.get`.
+            for event in tcod.event.wait():
+                context.convert_event(event)  # Sets tile coordinates for mouse events.
+                print(event)  # Print event names and attributes.
+                if isinstance(event, tcod.event.Quit):
+                    raise SystemExit()
+        # The window will be closed after the above with-block exits.
+
+
+if __name__ == "__main__":
+    main()
