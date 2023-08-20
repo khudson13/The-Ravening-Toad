a57aa40a3a13d238094fb56da64be1168eb2fddb
khudson13 <65306661+khudson13@users.noreply.github.com>
2023-08-19 21:25:08 -0700

    small details

--- The Ravening Toad/The Ravening Toad/Systems/Load.cs
+++ The Ravening Toad/The Ravening Toad/Systems/Load.cs
@@ -38,48 +38,61 @@ namespace The_Ravening_Toad.Systems
             {
                 if (bool.Parse(reader.ReadLine()))
                 {
-                    player.Name = reader.ReadLine();
-                    player.Attack = int.Parse(reader.ReadLine());
-                    player.AttackChance = int.Parse(reader.ReadLine());
-                    player.Awareness = int.Parse(reader.ReadLine());
-                    player.Defense = int.Parse(reader.ReadLine());
-                    player.DefenseChance = int.Parse(reader.ReadLine());
-                    player.Meat = int.Parse(reader.ReadLine());
-                    player.Cash = int.Parse(reader.ReadLine());
-                    player.Health = int.Parse(reader.ReadLine());
-                    player.MaxHealth = int.Parse(reader.ReadLine());
-                    player.X = int.Parse(reader.ReadLine());
-                    player.Y = int.Parse(reader.ReadLine());
-
-                    Game.seed = int.Parse(reader.ReadLine());
-                    Game.Random = new DotNetRandom(Game.seed);
-                    count = int.Parse(reader.ReadLine());
-                    Doors.Clear();
-                    for (int i = 0; i < count; ++i)
+                    try
                     {
-                        Doors.Add(new Door
+                        player.Name = reader.ReadLine();
+                        player.Attack = int.Parse(reader.ReadLine());
+                        player.AttackChance = int.Parse(reader.ReadLine());
+                        player.Awareness = int.Parse(reader.ReadLine());
+                        player.Defense = int.Parse(reader.ReadLine());
+                        player.DefenseChance = int.Parse(reader.ReadLine());
+                        player.Meat = int.Parse(reader.ReadLine());
+                        player.Cash = int.Parse(reader.ReadLine());
+                        player.Health = int.Parse(reader.ReadLine());
+                        player.MaxHealth = int.Parse(reader.ReadLine());
+                        if (player.Health > player.MaxHealth)
                         {
-                            X = int.Parse(reader.ReadLine()),
-                            Y = int.Parse(reader.ReadLine()),
-                            IsOpen = bool.Parse(reader.ReadLine())
-                        });
-                    }
-                    count = int.Parse(reader.ReadLine());
-                    Monsters.Clear();
-                    for (int i = 0; i < count; ++i)
-                    {
-                        string name = reader.ReadLine();
-                        switch (name)
-                        {
-                            case ("Dirty Rat"):
-                                var monster = DirtyRat.Create();
-                                monster.Health = int.Parse(reader.ReadLine());
-                                monster.X = int.Parse(reader.ReadLine());
-                                monster.Y = int.Parse(reader.ReadLine());
-                                Monsters.Add(monster);
-                                break;
+                            loading = false;
+                            return false;
                         }
-                        
+                        player.X = int.Parse(reader.ReadLine());
+                        player.Y = int.Parse(reader.ReadLine());
+
+                        Game.seed = int.Parse(reader.ReadLine());
+                        Game.Random = new DotNetRandom(Game.seed);
+                        count = int.Parse(reader.ReadLine());
+                        Doors.Clear();
+                        for (int i = 0; i < count; ++i)
+                        {
+                            Doors.Add(new Door
+                            {
+                                X = int.Parse(reader.ReadLine()),
+                                Y = int.Parse(reader.ReadLine()),
+                                IsOpen = bool.Parse(reader.ReadLine())
+                            });
+                        }
+                        count = int.Parse(reader.ReadLine());
+                        Monsters.Clear();
+                        for (int i = 0; i < count; ++i)
+                        {
+                            string name = reader.ReadLine();
+                            switch (name)
+                            {
+                                case ("Dirty Rat"):
+                                    var monster = DirtyRat.Create();
+                                    monster.Health = int.Parse(reader.ReadLine());
+                                    monster.X = int.Parse(reader.ReadLine());
+                                    monster.Y = int.Parse(reader.ReadLine());
+                                    Monsters.Add(monster);
+                                    break;
+                            }
+
+                        }
+                    }
+                    catch(FormatException)
+                    {
+                        loading = false;
+                        return false;
                     }
 
                     MapGenerator mapGenerator = new MapGenerator(Game.mapWidth, Game.mapHeight, 20, 13, 7, Game.mapLevel);
